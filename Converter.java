d56bb9915dbe2025ee7e0b88919347f60ca851ef
Marina <petrukmn@yandex.ru>
2020-05-18 13:26:40 +0300

    interface class

---
+++ Converter.java
@@ -0,0 +1,9 @@
+package kompjuterIya.lesson160520;
+
+public interface Converter {
+    void convertFromCToF(double c);
+    double convertFromFToC(double f);
+    void convertFromCToK(double c);
+    double convertFromKToC(double k);
+
+}
